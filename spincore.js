12e26416e21d16f32f86da56b2ef96968cdea511
Azaaxin <ludvig_olausson@hotmail.com>
2020-06-08 08:46:15 +0200

    Add files via upload

--- spincore.js
+++ spincore.js
@@ -1,10 +1,16 @@
 /*
-CommandList
+#Spin Command List
 
 $pin(selector).hide();                                             | Hides selected object
-$pin(selector).attr(name, value); | attr(name);                    | Sets attribute
-$pin(selector).getText(url, attribute); | .getText(url);           | Grabs the text from url, super basic ajax
-$pin(selector).hide();
+$pin(selector).attr(name, value);                                  | Sets attribute
+$pin(selector).getText(url, attribute);                            | Grabs the text from url, super basic ajax
+$pin(selector).phoneMenu(float, target);                           | Handles the positioning of hamburger menu's
+$pin(selector).random(value | empty);                              | Gives you a random number, default is 10.
+
+#Spincore Command List
+
+$pincore().random(value | empty)                                   | Gives you a random number, default is 10.
+
 */
 
 function $pin(selector)
@@ -17,20 +23,19 @@ function $pin(selector)
             document.addEventListener(event, callback)
         },
         hide:()=>{
-            self.Element.style.display = "none"
+            self.Element.style.display = "none";
         },
         attr:(name, value)=>{
             if(value == null)
-                self.Element.getAttribute(name)
+                self.Element.getAttribute(name);
             else
-                self.Element.setAttribute(name, value)
+                self.Element.setAttribute(name, value);
         },
         getText:(url, input)=>{
             var get = new XMLHttpRequest();
             get.onreadystatechange = function() {
               if (this.readyState == 4 && this.status == 200) {
                 self.Element.innerHTML += this.responseText;
-
               }
             };
             if(input == null){
@@ -80,58 +85,66 @@ function $pin(selector)
             get.send();
 
         },
+        random:(rand_floor)=>{
+            if(rand_floor == undefined)
+            self.Element.innerHTML += Math.floor(Math.random() * 10); 
+            else
+            self.Element.innerHTML += Math.floor(Math.random() * rand_floor); 
+        },
+        phoneMenu:(float, target)=>{
+            var element = document.getElementById(target);
+            var selc = document.getElementById(selector);
+            var positionInfo = element.getBoundingClientRect();
+            var height = positionInfo.height;
+            var width = positionInfo.width;
+            element.style.position = "absolute";
+            element.style.transition = ".3s";
+            if(float == "left"){
+                element.style.right = "100%";
+                selc.addEventListener("click", function(){
+                    element.style.position = "absolute";
+                    element.style.right = "calc(100%" + " - " + width + "px)";
+                    element.style.cursor = "pointer";
+                    element.style.transition = ".3s";
+                });
+            }else if(float == "right"){
+                element.style.left = "100%";
+                selc.addEventListener("click", function(){
+                    element.style.position = "absolute";
+                    element.style.left = "calc(100%" + " - " + width + "px)";
+                    element.style.cursor = "pointer";
+                });
+            }
+            if(float == "top"){
+                element.style.top = "-" + height + "px";
+                selc.addEventListener("click", function(){
+                    element.style.position = "absolute";
+                    element.style.top = "0px";
+                    element.style.cursor = "pointer";
+                    element.style.transition = ".3s";
+                });
+            }else if(float == "bottom"){
+                element.style.height = "0px";
+                element.style.bottom = "0px";
+                selc.addEventListener("click", function(){
+                    element.style.position = "absolute";
+                    element.style.height = height + "px";
+                    element.style.cursor = "pointer";
+                });
+            }
+        },
         
     }
     return self
 }
-// function $pinVal(){
-//     var valreturn = "";
-//     var me =
-//     {
-//         AjaxVal:(address, val_input, val_search)=>{
-//             var get = new XMLHttpRequest();
-//             get.onreadystatechange = function() {
-//               if (this.readyState == 4 && this.status == 200) {
-//                 object = JSON.parse(this.responseText);
-//                 valreturn = valfind(object, val_search);
-                
-//                 return valreturn
-//                 function valfind(object, valk) {
-//                     if(typeof(object) != 'object') {
-//                       return null;
-//                     }
-//                     var result = null;
-//                     if(object.hasOwnProperty(valk)) {
-//                         klj = object[valk];
-                        
-//                     } else {
-//                       for(var o in object) {
-//                         result = valfind(object[o], valk);
-//                         if(result == null) 
-//                             continue;
-//                         else 
-//                             break;
-//                       }
-//                     }
-//                     return klj;
-//                 }
-                  
-//               }
-              
-//             };
-//             if(val_input == null){
-//                 get.open("GET", address, true);
-//             }else if(val_input.substring(0, 1) == "?"){
-//                 get.open("GET", address + val_input, true);
-//             }else{
-//                 get.open("GET", address + "?" + val_input, true);
-//             }
-//             get.send();
-            
-//             me = valreturn
-//         }
-//     }
-//     return me
-    
-// }
-
+function $pincore(){   // Working through variables
+    var me = {};
+        me.random = function(floor){
+            if(floor == undefined)
+                me = Math.floor(Math.random() * 10); 
+            else
+                me = Math.floor(Math.random() * floor); 
+            return me;
+        }
+        return me;
+}
